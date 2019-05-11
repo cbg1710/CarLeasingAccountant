500e51c4ba5ae8b0504d2f6f675d5edb72951402
Christian Glaesel <cglaesel@hotmail.de>
2019-05-11 21:12:43 +0200

    Fixed run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-read -s “Directory where to save the vehicle information: ” dir
+echo -n "Enter directory where to save vehicle data and press [ENTER]: "
+read dir
 
 docker run -p 8080:8080 -v $dir:/vehicles -t glaesec/leasing-accountant:latest
